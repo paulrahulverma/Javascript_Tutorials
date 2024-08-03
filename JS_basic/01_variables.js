169d435efff3011e70012372251950326dd37f59
Rahul Verma <rahul.verma.2612@gmail.com>
2024-08-03 17:33:17 +0000

    create variables

---
+++ JS_basic/01_variables.js
@@ -0,0 +1,7 @@
+const applicantName = "Rahul Verma";
+let applicantAge = 36
+let applicantCity = "Ghaziabad";
+let applicantState = "Uttar Pradesh";
+const applicantCountry = "India";
+
+console.log([applicantName, applicantAge, applicantCity, applicantState, applicantCountry])
