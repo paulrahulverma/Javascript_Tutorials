a4095ca02c4e7beefc89d7141560efcb1df69e9d
Rahul Verma <rahul.verma.2612@gmail.com>
2024-08-06 14:21:14 +0000

    datatype

---
+++ JS_basic/03_datatype.js
@@ -0,0 +1,22 @@
+let fullName = "Rahul verma";
+// console.log(fullName);
+// String
+
+let myAge = 36;
+//console.log(Number(myAge));
+// Number
+
+let myNumber = true;
+// console.log(typeof(myNumber));
+// Boolean
+
+let myStatus;
+// console.log(typeof(myStatus));
+// Undefined
+
+let myHugeNum = 9711914565n;
+// console.log(typeof(myHugeNum));
+// Null
+
+let noThing = `Null`
+console.log(noThing);
